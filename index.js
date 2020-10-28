414eb537b0736c7f9b2827375ceb1a81f7c06ca3
Impress Flows <nyangiwy24@naver.com>
2020-10-28 21:34:25 +0900

    d

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ var app = http.createServer(function(request,response){
     response.end(fs.readFileSync(__dirname + url));
  
 });
-app.listen(3000); 
+app.listen(3000, () => {
+  console.log("port 3000\n\ngo localhost:3000/playground")
+}); 
